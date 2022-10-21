a211effc1d69896e45898eeebbc7291e2e1b899a
Juan Camilo Castro Pinto <jccastrop@unal.edu.co>
2022-10-20 21:58:33 -0500

    Adicion de pruebas de servicios web y clases del modelo

--- main.py
+++ main.py
@@ -5,4 +5,39 @@ from flask_cors import CORS
 import json
 from waitress import serve
 
-print("Flask desde cero")
+app = Flask(__name__)
+cors = CORS(app)
+
+def load_file_config():
+  with open("config.json") as f:
+    data = json.load(f)
+  return data
+
+@app.route("/suma", methods=["POST"])
+def suma():
+  jj = request.get_json()
+  res = jj["n1"] + jj["n2"]
+  data = { "result" : res }
+  return jsonify(data)
+
+
+@app.route("/post", methods=["POST"])
+def test():
+  data = { "message" : "The server is running (POST)...." }
+  return jsonify(data)
+
+@app.route("/put", methods=["PUT"])
+def test1():
+  data = { "message" : "The server is running (PUT)...." }
+  return jsonify(data)
+
+@app.route("/get", methods=["GET"])
+def test2():
+  data = { "message" : "The server is running (GET)...." }
+  return jsonify(data)
+
+
+if __name__ == "__main__":
+  data_config = load_file_config()
+  print(f"Server running : http://{data_config['url-backend']}:{data_config['port']}")
+  serve(app, host=data_config["url-backend"], port=data_config["port"])
