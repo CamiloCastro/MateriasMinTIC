7c47bbade9f7b90839cb4039b98d9609df9257f8
Juan Camilo Castro Pinto <jccastrop@unal.edu.co>
2022-10-21 21:23:47 -0500

    Creacion del controlador de estudiante

--- main.py
+++ main.py
@@ -4,14 +4,43 @@ from flask import request
 from flask_cors import CORS
 import json
 from waitress import serve
+from controller.estudiante_controller import ControladorEstudiante
 
 app = Flask(__name__)
 cors = CORS(app)
+estudiante_controller = ControladorEstudiante()
 
 def load_file_config():
   with open("config.json") as f:
     data = json.load(f)
   return data
+
+@app.route("/estudiantes", methods=["GET"])
+def listar_estudiante():
+  lista_estudiantes = estudiante_controller.index()
+  return jsonify(lista_estudiantes)
+
+@app.route("/estudiante", methods=["POST"])
+def crear_estudiante():
+  info_estudiante = request.get_json()
+  estudiante_creado = estudiante_controller.create(info_estudiante)
+  return jsonify(estudiante_creado)
+
+@app.route("/estudiante/<string:id>", methods=["GET"])
+def mostrar_estudiante(id):
+  est = estudiante_controller.show(id)
+  return jsonify(est)
+
+@app.route("/estudiante/<string:id>", methods=["PUT"])
+def actualizar_estudiante(id):
+  info_estudiante = request.get_json()
+  estudiante_actualizado = estudiante_controller.update(id, info_estudiante)
+  return jsonify(estudiante_actualizado)
+
+@app.route("/estudiante/<string:id>", methods=["DELETE"])
+def eliminar_estudiante(id):
+  resp = estudiante_controller.delete(id)
+  return jsonify(resp)
 
 @app.route("/suma", methods=["POST"])
 def suma():
@@ -19,7 +48,6 @@ def suma():
   res = jj["n1"] + jj["n2"]
   data = { "result" : res }
   return jsonify(data)
-
 
 @app.route("/post", methods=["POST"])
 def test():
